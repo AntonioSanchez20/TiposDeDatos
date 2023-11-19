48f81eecdabdea4ad6c3be590c1b615a47f58b40
AntonioSanchez20 <98866955+AntonioSanchez20@users.noreply.github.com>
2023-11-19 00:32:31 -0600

    Add files via upload

---
+++ TiposDeDatos/TiposDeDatos/Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Dim oNac As Object
+        'Dim strValor As String
+        'asignamos una cadena de texto al tipo de datos strValor
+        'strValor = "VISUAL BASIC 2023"
+
+        'asignamos una fehca al tipo de dato oNac
+        'oNac = "09/07/1926"
+
+        'asignamos una cadena de texto al tipo de dato oNac
+        'oNac = "Julio"
+
+        'Numero enteros positivos de 0 - 255
+        'Dim b As Byte = 255
+
+        'Numeros enteros desde -2.147.483.648 a 2.147.483.647
+        'Dim i As Integer = 2147483647
+
+        'Números enteros desde -9.223.372.036.854.775.808 a (9.2E+18)
+        'Dim l As Long = 9223372036854775807
+
+        'Dim l1 As Long
+        'Dim s2 As String
+        'l1 = 12345
+        's2 = “Nicolas”
+
+        'Dim bResp As Boolean, a As Long
+
+        'a = 1234
+
+        'If a = 1234 Then bResp = True Else bResp = False
+        ''Si a = 1234 entonces bResp es verdadero, caso contrario bResp es falso
+
+        'MsgBox(bResp.ToString)
+
+        'Dim sNumero As Single
+        'sNumero.MinValue
+        'sNumero.MaxValue
+
+
+    End Sub
+End Class
